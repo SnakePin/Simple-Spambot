b13fe175e09a43be5def4baa0b8ab7ca163ae3a7
SnakePin <18491360+SnakePin@users.noreply.github.com>
2019-03-12 20:52:21 +0300

    First commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start F5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(528, 168);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(521, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop F6";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Delay(MS):";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(68, 16);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(88, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Text:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(162, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Use ClipBoard Lib";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(278, 17);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(152, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Discord Edit Last Message";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 225);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SpamBot by SnakePin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        InputSimulator sim = new InputSimulator();
+        bool stop = true;
+        string spamtext = null;
+        int delayms = 5;
+        bool usecliplib = false;
+        bool discordEdtLastmsg = false;
+        int animvar1;
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void interpretText()
+        {
+            spamtext = textBox1.Text;
+            spamtext = spamtext.Replace("{TIME}", DateTime.Now.ToString("HH:mm:ss"));
+            spamtext = spamtext.Replace("{RANDOM}", RandomString(10));
+            switch (animvar1)
+            {
+                case 0:
+                    spamtext = spamtext.Replace("{LOADING}", "[..........]");
+                    animvar1 = 10;
+                    break;
+                case 10:
+                    spamtext = spamtext.Replace("{LOADING}", "[=.........]");
+                    animvar1 = 20;
+                    break;
+                case 20:
+                    spamtext = spamtext.Replace("{LOADING}", "[==........]");
+                    animvar1 = 30;
+                    break;
+                case 30:
+                    spamtext = spamtext.Replace("{LOADING}", "[===.......]");
+                    animvar1 = 40;
+                    break;
+                case 40:
+                    spamtext = spamtext.Replace("{LOADING}", "[====......]");
+                    animvar1 = 50;
+                    break;
+                case 50:
+                    spamtext = spamtext.Replace("{LOADING}", "[=====.....]");
+                    animvar1 = 60;
+                    break;
+                case 60:
+                    spamtext = spamtext.Replace("{LOADING}", "[======....]");
+                    animvar1 = 70;
+                    break;
+                case 70:
+                    spamtext = spamtext.Replace("{LOADING}", "[=======...]");
+                    animvar1 = 80;
+                    break;
+                case 80:
+                    spamtext = spamtext.Replace("{LOADING}", "[========..]");
+                    animvar1 = 90;
+                    break;
+                case 90:
+                    spamtext = spamtext.Replace("{LOADING}", "[=========.]");
+                    animvar1 = 100;
+                    break;
+                case 100:
+                    spamtext = spamtext.Replace("{LOADING}", "[==========]");
+                    animvar1 = 0;
+                    break;
+                default:
+                    spamtext = spamtext.Replace("{LOADING}", "[..........]");
+                    animvar1 = 0;
+                    break;
+
+            }
+        }
+
+        public void Spam()
+        {
+            Console.WriteLine("[INFO] Spam starting in 5.");
+            Thread.Sleep(5000);
+            if (!usecliplib)
+            {
+                while (!stop)
+                {
+                    interpretText();
+                    if (discordEdtLastmsg)
+                    {
+                        sim.Keyboard.KeyPress(VirtualKeyCode.UP);
+                        Thread.Sleep(1);
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
+                        Thread.Sleep(1);
+                    }
+                    sim.Keyboard.TextEntry(spamtext);
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    Thread.Sleep(delayms);
+                }
+            }
+            else
+            {
+                while (!stop)
+                {
+                    interpretText();
+                    Thread thread = new Thread(() => Clipboard.SetText(spamtext));
+                    thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+                    thread.Start();
+                    thread.Join();
+                    if (discordEdtLastmsg)
+                    {
+                        sim.Keyboard.KeyPress(VirtualKeyCode.UP);
+                        Thread.Sleep(1);
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
+                        Thread.Sleep(1);
+                    }
+                    sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    Thread.Sleep(delayms);
+                }
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+            stop = false;
+            Thread spamThread = new Thread(new ThreadStart(Spam));
+            spamThread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = delayms;
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip1.SetToolTip(this.checkBox1, "Better and faster for long texts but not for short texts, but minimum delay is 100ms if enabled.");
+            toolTip1.SetToolTip(this.checkBox2, "If enabled, edits last message instead of sending new message, but minimum delay is 500ms if enabled to bypass discord restrictions.");
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (delayms < 5 && !usecliplib)
+                numericUpDown1.Value = 5;
+            else if (delayms < 100 && usecliplib)
+                numericUpDown1.Value = 100;
+
+            delayms = (int)numericUpDown1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            stop = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            usecliplib = checkBox1.Checked;
+            if (delayms < 100 && usecliplib)
+                numericUpDown1.Value = 100;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            discordEdtLastmsg = checkBox1.Checked;
+            if (delayms < 500 && usecliplib)
+                numericUpDown1.Value = 500;
+
+        }
+    }
+}
